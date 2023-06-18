e6f3458b520ae158fec36eb48a5654b8635d2d49
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-06-18 22:24:27 +0500

    multiple ele passed in compnt.

--- src/User.js
+++ src/User.js
@@ -3,6 +3,7 @@ function User() {
   return (
     <div className="App">
       <h1>Welcome Users</h1>
+      <h3>Multiple element via component must wrapped in single div, bcz only one wrapped element should be passed via Components.</h3>
     </div>
   );
 }
